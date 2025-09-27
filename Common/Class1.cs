4e558782cc2ae3832f4ab91eb763fb2161aae806
DrOrange01 <20nikola01s@gmail.com>
2025-09-27 17:14:45 +0200

    didat solution koji je falio

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 
 namespace Common
 {
+    [DataContract]
+    public enum nesto
+    {
+        [EnumMember]prvi,
+        [EnumMember]drugi
+    }
+    [DataContract]
     public class Class1
     {
+        [DataMember]
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
-namespace Server
+namespace Service
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(LibraryService));
+            host.Open();
+
+            Console.WriteLine("Service is running...");
+            Console.ReadKey();
+
+            host.Close();
+            Console.WriteLine("Service is closed.");
         }
     }
 }
